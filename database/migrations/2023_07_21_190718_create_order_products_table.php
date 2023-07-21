81d81071dad6d6b5ff259c5d24d7efa1422bb783
Hossam <housamelmasry@icloud.com>
2023-07-21 23:50:58 +0300

    Added Order Model

--- database/migrations/2023_07_21_190718_create_order_products_table.php
+++ database/migrations/2023_07_21_190718_create_order_products_table.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Order;
+use App\Models\Project;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -12,8 +14,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('order_products', function (Blueprint $table) {
-            $table->id();
+            $table->foreignId('order_id')->references('id')->on('orders');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->integer('quantity');
+            $table->decimal('unit_price', 10, 2);
+            $table->decimal('total_price',);
             $table->timestamps();
+
+
         });
     }
 
