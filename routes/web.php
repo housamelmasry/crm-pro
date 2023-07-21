7f81c8c698fd22b95232556aeac784c16d8db69c
mohamedemasry <134013462+mohamedemasry@users.noreply.github.com>
2023-07-21 22:55:24 +0300

    Added Comment to web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//test commit
 
 Route::get('/', function () {
     return view('welcome');
